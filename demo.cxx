93c8661397fd89032b17d07cc5e4f96802e326b0
Excelsior <null@std.uestc.edu.cn>
2020-06-09 11:34:29 +0800

    update demo

--- demo.cxx
+++ demo.cxx
@@ -1,14 +1,8 @@
 #include "liblpt1602.h"
-#include <stdio.h>
 
+#include <stdio.h>
 #include <sys/sysinfo.h>
 #include <unistd.h>
-/*
-#include <netdb.h>
-#include <ifaddrs.h>
-#include <stdlib.h>
-#include <linux/if_link.h>
-*/
 #include <string.h> /* for strncpy */
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -16,6 +10,8 @@
 #include <netinet/in.h>
 #include <net/if.h>
 #include <arpa/inet.h>
+
+#include <ctime>
 #include <string>
 #include <iomanip>
 #include <array>
@@ -25,6 +21,7 @@
 int cpuPg(std::string);
 int netPg(std::string);
 int memPg(std::string);
+int timPg(std::string);
 unsigned int getXbytes(std::string interface, std::string xDir);
 std::string getXunit(double &xSpd);
 
@@ -33,11 +30,11 @@ constexpr unsigned int KiB = 1<<10;
 constexpr unsigned int MiB = 1<<20;
 constexpr unsigned int GiB = 1<<30;
 lpt1602 npt;
-constexpr std::array<int(*)(std::string), 3> cbArr{cpuPg, memPg, netPg};
+constexpr std::array<int(*)(std::string), 4> cbArr{cpuPg, memPg, netPg, timPg};
 
 constexpr std::array<char, 8> upArr{0x00, 0x00, 0x04, 0x0a, 0x11, 0x00, 0x00, 0x00};
 constexpr std::array<char, 8> dnArr{0x00, 0x00, 0x11, 0x0a, 0x04, 0x00, 0x00, 0x00};
-constexpr std::array<char, 8> cDeg {0x16, 0x09, 0x08, 0x08, 0x08, 0x09, 0x06, 0x00};
+constexpr std::array<char, 8> cDeg {0x02, 0x05, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 int cpuPg(std::string dumb){	
 	struct sysinfo info;
@@ -60,8 +57,8 @@ int cpuPg(std::string dumb){
 	n = fscanf(thermal, "%f", &tmp2);
 	fclose(thermal);
 	std::stringstream tem;
-	tem <<"  "<<std::fixed<<std::setprecision(1)
-		<<tmp1/1000<<(char)0x03<<"  "<<tmp2/1000<<(char)0x03;
+	tem <<" "<<std::fixed<<std::setprecision(1)
+		<<tmp1/1000<<(char)0x02<<'C'<<"  "<<tmp2/1000<<(char)0x02<<'C';
 	npt.curPos(2, 0);
 	npt.puts(tem.str().c_str());
 	return 0;
@@ -70,53 +67,49 @@ int cpuPg(std::string dumb){
 int memPg(std::string dumb){	
 	struct sysinfo info;
 	const int ret = sysinfo(&info);	
+	std::stringstream tsk;
+	tsk <<"   "<<std::setw(4)<<info.procs<<"procs";
+	npt.clear();
+	npt.curPos(1, 0);
+	npt.puts(tsk.str().c_str());
 	std::stringstream mem;
-	mem <<"Mem "<<std::fixed<<std::setprecision(1)
-		<<static_cast<double>(info.totalram - info.freeram)/(MiB)<<'/'
-		<<static_cast<double>(info.totalram)/(MiB);
+	double usd = static_cast<double>(info.totalram - info.freeram);
+	double utPer = usd/static_cast<double>(info.totalram)*100;
+	std::string uUnt = getXunit(usd);
+	mem <<"Mem "<<std::fixed<<std::setprecision(1)<<usd<<uUnt<<' '<<utPer<<'%';
 	npt.curPos(2, 0);
 	npt.puts(mem.str().c_str());
 	return 0;
 }
 
-int netPg(std::string interface = std::string{"eth0"}){	
-	/*
-	struct ifaddrs *ifaddr, *ifa;
-	int family, s, n;
-	char host[NI_MAXHOST];
-	if (getifaddrs(&ifaddr) == -1) {	           
-		perror("getifaddrs");
-		exit(EXIT_FAILURE);
-	}
-	*/
-	/* Walk through linked list, maintaining head pointer so we
-	 * can free list later */
-	/*
-	for (ifa = ifaddr, n = 0; ifa != NULL; ifa = ifa->ifa_next, n++) {	
-		if (ifa->ifa_addr == NULL || std::string(ifa->ifa_name) != interface)
-			continue;
-		family = ifa->ifa_addr->sa_family;
-		if (family == AF_PACKET && ifa->ifa_data != NULL) {	    
-	        struct rtnl_link_stats *stats = (struct rtnl_link_stats *)ifa->ifa_data;
-			printf("\t\ttx_packets = %10u; rx_packets = %10u\n"
-			"\t\ttx_bytes   = %10u; rx_bytes   = %10u\n",
-			stats->tx_packets, stats->rx_packets,
-	        stats->tx_bytes, stats->rx_bytes);
-			unsigned int txp = stats->tx_bytes;
-			unsigned int rxp = stats->rx_bytes;
-			usleep(500000);
-			double txSpd = (stats->tx_bytes - txp)/0.5;
-			double rxSpd = (stats->rx_bytes - rxp)/0.5;
-			std::stringstream spd;
-			spd<<"TX: "<<std::fixed<<std::setprecision(9)<<txSpd<<"Bps";
-			npt.clear();
-			npt.curPos(1, 0);
-			npt.puts(spd.str().c_str());
-		}
-	}
+int timPg(std::string dumb){	
+	time_t now = time(0);
+	tm *ltm = localtime(&now);
+	std::stringstream ntm;
+	ntm <<' '<<std::setw(2)<<std::setfill('0')<<ltm->tm_year % 100<<'/'
+		<<std::setw(2)<<std::setfill('0')<<ltm->tm_mon<<'/'
+		<<std::setw(2)<<std::setfill('0')<<ltm->tm_mday<<' '
+		<<std::setw(2)<<std::setfill('0')<<ltm->tm_hour<<':'
+		<<std::setw(2)<<std::setfill('0')<<ltm->tm_min;
 	npt.clear();
 	npt.curPos(1, 0);
-	*/
+	npt.puts(ntm.str().c_str());
+	struct sysinfo info;
+	const int ret = sysinfo(&info);
+	std::stringstream utm;
+	time_t upt = static_cast<time_t>(info.uptime);
+	ltm = localtime(&upt);
+	utm <<std::setw(2)<<std::setfill('0')<<ltm->tm_mon<<'M'
+		<<std::setw(2)<<std::setfill('0')<<ltm->tm_mday<<'D'
+		<<std::setw(2)<<std::setfill('0')<<ltm->tm_hour<<'h'
+		<<std::setw(2)<<std::setfill('0')<<ltm->tm_min<<'m'
+		<<std::setw(2)<<std::setfill('0')<<ltm->tm_sec<<'s';
+	npt.curPos(2, 0);
+	npt.puts(utm.str().c_str());
+	return 0;
+}
+
+int netPg(std::string interface = std::string{"eth0"}){	
 	int fd;
 	struct ifreq ifr;
 	fd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -128,7 +121,9 @@ int netPg(std::string interface = std::string{"eth0"}){
 	close(fd);
 	npt.clear();
 	npt.curPos(1, 0);
-	npt.puts(inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+	std::string ipAddr(inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+	int padd = (16 - ipAddr.size())/2;
+	npt.puts((std::string(padd, ' ') + ipAddr).c_str());
 	const int rxTmp = getXbytes(interface, std::string("rx_bytes"));
 	const int txTmp = getXbytes(interface, std::string("tx_bytes"));
 	usleep(50000);
@@ -137,11 +132,12 @@ int netPg(std::string interface = std::string{"eth0"}){
 	std::string txUnit = getXunit(txSpd);
 	std::string rxUnit = getXunit(rxSpd);
 	std::stringstream spd;
-	spd <<(char)0x58<<std::fixed<<std::setprecision(1)<<std::setw(5)<<std::setfill(' ')
-		<<txSpd<<txUnit
-		<<' '<<(char)0x60<<std::setprecision(2)<<std::setw(6)<<std::setfill(' ')
-		<<rxSpd<<rxUnit;
+	spd <<(char)0x03<<std::left<<std::fixed<<std::setprecision(2)<<txSpd<<txUnit;
 	npt.curPos(2, 0);
+	npt.puts(spd.str().c_str());
+	spd.str("");
+	spd	<<' '<<(char)0x04<<std::setprecision(2)<<rxSpd<<rxUnit;
+	npt.curPos(2, 7);
 	npt.puts(spd.str().c_str());
 	return 0;
 }
@@ -179,7 +175,7 @@ bool thd(){
 	npt.tgDsp(1);
 	do{	
 		if(ext) return (ext = false);					/*clear exit flag*/
-		cbArr[tsk]("eth0");
+		cbArr[tsk]("eth1");
 		sleep(3);
 		npt.clear();
 	}while(++tsk != cbArr.size());
@@ -193,7 +189,7 @@ int main(void){
 	npt.defCh(0x58, upArr);
 	npt.defCh(0x60, dnArr);
 	npt.defCh(0x50, cDeg);
-	thd();
+	while(1)thd();
 	while(1){	
 		if(((inb(0x378 + 1)^0x80) >> 3) & (1 << 3)){	
 			usleep(200000);
